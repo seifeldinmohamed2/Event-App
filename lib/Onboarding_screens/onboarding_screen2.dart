475a5970f5c7246b3c57cdda004c7bb80dda5581
Lenovo <eldenseif341@gmail.com>
2025-08-07 06:29:17 +0300

    push any way

--- lib/Onboarding_screens/onboarding_screen2.dart
+++ lib/Onboarding_screens/onboarding_screen2.dart
@@ -12,70 +12,72 @@ class OnboardingScreen2 extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColors.ofwhite,
 
-      body: Column(
-        children: [
-          SizedBox(
-            height:100,
-          ),
-          Container(
-            child: Center(child: Image.asset('assets/images/onbord2.png')),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height:100,
+            ),
+            Container(
+              child: Center(child: Image.asset('assets/images/onbord2.png')),
 
-          ),
-          SizedBox(
-            height: 50,
-          ),
-          Stack(
-            alignment: Alignment.center,
-            children: [
-              Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                    padding: const EdgeInsets.only(right: 16),
-                    child: FilledButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => const OnboardingScreen1()),
-                        );                      },
-                      style: FilledButton.styleFrom(
-                        foregroundColor: AppColors.ofwhite,
-                        backgroundColor: AppColors.ofwhite,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Align(
+                    alignment: Alignment.centerLeft,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 16),
+                      child: FilledButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => const OnboardingScreen1()),
+                          );                      },
+                        style: FilledButton.styleFrom(
+                          foregroundColor: AppColors.ofwhite,
+                          backgroundColor: AppColors.ofwhite,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16),
+                          ),
                         ),
+                        child: Image.asset('assets/images/Group 10.png'),
                       ),
-                      child: Image.asset('assets/images/Group 10.png'),
-                    ),
-                  )
-              ),
-              Align(
-                alignment: Alignment.center,
-                child: Image.asset('assets/images/Group 5.png'),
-              ),
-              Align(
-                  alignment: Alignment.centerRight,
-                  child: Padding(
-                    padding: const EdgeInsets.only(right: 16),
-                    child: FilledButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => const OnboardingScreen3()),
-                        );                      },
-                      style: FilledButton.styleFrom(
-                        foregroundColor: AppColors.ofwhite,
-                        backgroundColor: AppColors.ofwhite,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16),
+                    )
+                ),
+                Align(
+                  alignment: Alignment.center,
+                  child: Image.asset('assets/images/Group 5.png'),
+                ),
+                Align(
+                    alignment: Alignment.centerRight,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 16),
+                      child: FilledButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => const OnboardingScreen3()),
+                          );                      },
+                        style: FilledButton.styleFrom(
+                          foregroundColor: AppColors.ofwhite,
+                          backgroundColor: AppColors.ofwhite,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16),
+                          ),
                         ),
+                        child: Image.asset('assets/images/Group 6.png'),
                       ),
-                      child: Image.asset('assets/images/Group 6.png'),
-                    ),
-                  )
-              ),
-            ],
-          ),
-        ],
+                    )
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
 
     );
